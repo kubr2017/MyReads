6e5971c9046b9a9dd9721fee91db4929ec58cc55
kubr2017 <kubr2017@gmail.com>
2018-12-21 11:52:37 -0500

    feat: introduce DropDownList in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import { DropDownList }'./components/DropDownList'
 
 class BooksApp extends React.Component {
 
@@ -54,6 +55,8 @@ class BooksApp extends React.Component {
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                            <DropDownList/>
+
                             // <div className="book-shelf-changer">
                             //   <select>
                             //     <option value="move" disabled>Move to...</option>
