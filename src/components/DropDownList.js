b24324d71f614ecea1b2bcd3fce266b5e13b4cb4
kubr2017 <kubr2017@gmail.com>
2018-12-21 12:09:28 -0500

    fix: add key to option.

--- src/components/DropDownList.js
+++ src/components/DropDownList.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 class DropDownList extends React.Component {
   renderDropDownList = () => {
-    const { dropDownDownData } = this.props;
+    const { dropDownDownData } = this.props
     return (
       <div className="book-shelf-changer">
         <select>
           dropDownDownData.map(function(item){
-            <option value={item}>{item}</option>
+            <option key = {item} value={item}>{item}</option>
           })
         </select>
       </div>
