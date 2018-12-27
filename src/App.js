511860c94b37df8a660e996a613d8716c0643ec1
kubr2017 <kubr2017@gmail.com>
2018-12-27 13:11:08 -0500

    feat:fetch books async-ly and put to array

--- src/App.js
+++ src/App.js
@@ -23,11 +23,17 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
-    const books = []
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((res) =>
+      this.setState({books:res}) )
   }
 
   render() {
+    console.log(this.state.books);
     return (
       <div className="app">
         {this.state.showSearchPage ? (
