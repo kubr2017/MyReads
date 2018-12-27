16ed767fa8724a7bf62427cff0060fb8d1a980ed
kubr2017 <kubr2017@gmail.com>
2018-12-27 08:07:13 -0500

    feat: move data out component body

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { DropDownList } from './DropDownList'
 import * as BooksAPI from '../../src/BooksAPI.js'
 
+const urlPath = "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
+const title = 'The Hobbit'
+const author = 'J.R.R. Tolkien'
 
 class Book extends React.Component {
   // const res = {};
@@ -11,11 +14,11 @@ class Book extends React.Component {
       <div>
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
+          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${urlPath})`}}></div>
           <DropDownList/>
         </div>
-        <div className="book-title">The Hobbit</div>
-        <div className="book-authors">J.R.R. Tolkien</div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{author}</div>
       </div>
       <div>
       {/*BooksAPI.getAll().then(function(res){console.log(res)})*/}
