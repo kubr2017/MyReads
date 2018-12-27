3c6f1f2a1e2ae5abf1fb496b2d0a54bc22a4cac5
kubr2017 <kubr2017@gmail.com>
2018-12-27 12:43:49 -0500

    feat: add Shelfs array and import BooksAPI in App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css'
 import { DropDownList } from './components/DropDownList'
 import { Book } from './components/Book'
 import { Shelf } from './components/Shelf'
+import * as BooksAPI from '../../src/BooksAPI.js'
+
+const Shelfs  = ['Currently Reading','Want to Read','Read']
+
 
 //const data = [{urlPath:"http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
   //       title:'The Hobbit',
