9c31f323e75ec1df6439ab901151a2c8d5799641
kubr2017 <kubr2017@gmail.com>
2018-12-27 11:44:03 -0500

    feat: Implement book component
    book component responsible for rendering single book. Get data through 
    props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css'
 import { DropDownList } from './components/DropDownList'
 import { Book } from './components/Book'
 
+const data = [{urlPath:"http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
+         title:'The Hobbit',
+         author:'J.R.R. Tolkien'}]
+
 class BooksApp extends React.Component {
 
 
@@ -73,7 +77,7 @@ class BooksApp extends React.Component {
                         </div>
                       </li>
                       <li>
-                        <Book/>
+                        <Book data={data}/>
                       </li>
                     </ol>
                   </div>
--- src/components/Book.js
+++ src/components/Book.js
@@ -2,28 +2,30 @@ import React from 'react'
 import { DropDownList } from './DropDownList'
 import * as BooksAPI from '../../src/BooksAPI.js'
 
-const urlPath = "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api"
-const title = 'The Hobbit'
-const author = 'J.R.R. Tolkien'
+
 
 class Book extends React.Component {
   // const res = {};
 
   render() {
+    console.log('prop',this.props.data);
     return (
-      <div>
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${urlPath})`}}></div>
-          <DropDownList/>
+      this.props.data.map((item,index) => (
+        <div key={index}>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${item.urlPath})`}}></div>
+            <DropDownList/>
+          </div>
+          <div className="book-title">{item.title}</div>
+          <div className="book-authors">{item.author}</div>
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">{author}</div>
-      </div>
-      <div>
-      {/*BooksAPI.getAll().then(function(res){console.log(res)})*/}
-      </div>
-      </div>
+        <div>
+        {/*BooksAPI.getAll().then(function(res){console.log(res)})*/}
+        </div>
+        </div>)
+      )
+
     )
   }
 }
