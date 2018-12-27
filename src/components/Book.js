8d08cf6da356709eeedf2d8e411d739079cfead7
kubr2017 <kubr2017@gmail.com>
2018-12-27 07:16:31 -0500

    chore: comments {getAll()...}

--- src/components/Book.js
+++ src/components/Book.js
@@ -18,7 +18,7 @@ class Book extends React.Component {
         <div className="book-authors">J.R.R. Tolkien</div>
       </div>
       <div>
-      {getAll().then(function(res){console.log(res)})}
+      {/*getAll().then(function(res){console.log(res)})*/}
       </div>
       </div>
     )
