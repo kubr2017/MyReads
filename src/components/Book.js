5a3055ab6b059db89cdd6237f2c591eef126fec3
kubr2017 <kubr2017@gmail.com>
2019-01-01 15:40:48 -0500

    fix: got thumbnails from imageLinks by this.props.data.imageLinks && this.props.data.imageLinks.thumbnail

--- src/components/Book.js
+++ src/components/Book.js
@@ -7,25 +7,27 @@ import * as BooksAPI from '../../src/BooksAPI.js'
 class Book extends React.Component {
   // const res = {};
   // state = {
-  //   urlPath: this.props.data.map((item)=>(item.imageLinks.thumbnail)),
-  //   title: this.props.data.map((item)=>(item.title)),
-  //   Author:[]
-  //
-  // }
+//     urlPath: this.props.data.imageLinks.thumbnail ? this.props.data.imageLinks.thumbnail: "https://www.computerhope.com/jargon/e/error.gif"
+
+//   }
 
   render() {
-    console.log('prop',this.props.data.imageLinks)
+    //console.log('props:',this.props.data)
+    //console.log('imageLinks',(this.props.data.imageLinks&&this.props.data.imageLinks.thumbnail) || null )
+    //console.log('title',this.props.data.title? this.props.data.title:null)
+    //console.log('authors',this.props.data.authors ? this.props.data.authors[0]:null)
     {/*this.props.data ? console.log('map:',this.props.data.title):console.log('zero');*/}
 
     return (
         <div key={this.props.data.id}>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage:`url(${/*"https://www.computerhope.com/jargon/e/error.gif"*/this.props.data.imageLinks})` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage:`url(${(this.props.data.imageLinks && this.props.data.imageLinks.thumbnail) || 'https://www.computerhope.com/jargon/e/error.gif'})` }}></div>
             <DropDownList/>
           </div>
-          <div className="book-title">{this.props.data.title}</div>
-          <div className="book-authors">{this.props.data.author}</div>
+          <div className="book-title">{this.props.data.title && this.props.data.title || null}</div>
+          {/*(this.props.data.author) && (this.props.data.author.map((item) => (<div className="book-authors">{item}</div>)))*/}
+          <div className="book-authors">{this.props.data.author && this.props.data.author[0] || null}</div>
         </div>
         <div>
         {/*BooksAPI.getAll().then(function(res){console.log(res)})*/}
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -4,8 +4,20 @@ import {Book} from './Book'
 
 class Shelf extends React.Component {
 
+
+      state = {
+        a: []
+      }
+
   render(){
     console.log('shelf comp booksArr:',this.props.booksArr);
+    console.log('shelf comp booksArr[0]:',this.props.booksArr[0]);
+  //  {const a = this.props.booksArr[0];
+  //  console.log('a:',a);}
+    this.state.a = this.props.booksArr[0]
+
+    console.log('a:',this.state.a);
+
     return(
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.data/*Currently Reading*/}</h2>
