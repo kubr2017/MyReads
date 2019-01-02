b3feebbd7d81206e2fbf630ff64b18445afedde6
kubr2017 <kubr2017@gmail.com>
2019-01-01 19:45:24 -0500

    feat: Change Shelf component.
    Get rid from redudant <div book-shelf> and <h2 title='Curently reading'>

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -4,33 +4,26 @@ import {Book} from './Book'
 
 class Shelf extends React.Component {
 
-
       state = {
         a: []
       }
 
   render(){
-    console.log('shelf comp booksArr:',this.props.booksArr);
-    console.log('shelf comp booksArr[0]:',this.props.booksArr[0]);
+  //  console.log('shelf comp booksArr:',this.props.booksArr);
+  //  console.log('shelf comp booksArr[0]:',this.props.booksArr[0]);
   //  {const a = this.props.booksArr[0];
   //  console.log('a:',a);}
     this.state.a = this.props.booksArr[0]
-
     console.log('a:',this.state.a);
 
     return(
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.data/*Currently Reading*/}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
               {this.props.booksArr.map((item)=>(<li><Book data={item}/></li>))}
           </ol>
         </div>
-      </div>
     )
   }
-
-
 }
 
 export { Shelf }
