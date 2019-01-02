32c9de6641133599e3c7cf911df48fc672d547c6
kubr2017 <kubr2017@gmail.com>
2019-01-01 23:40:52 -0500

    chore: Comment console.log in <Book/>

--- src/components/Book.js
+++ src/components/Book.js
@@ -15,7 +15,7 @@ class Book extends React.Component {
     //console.log('imageLinks',(this.props.data.imageLinks&&this.props.data.imageLinks.thumbnail) || null )
     //console.log('title',this.props.data.title? this.props.data.title:null)
     //console.log('authors',this.props.data.authors ? this.props.data.authors[0]:null)
-    console.log('shelf',this.props.data.shelf ? this.props.data.shelf:null)
+    //console.log('shelf',this.props.data.shelf ? this.props.data.shelf:null)
 
     return (
         <div key={this.props.data.id}>
