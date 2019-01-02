38a48d63fa7675c1e67f8eabd7363c0a1e81f585
kubr2017 <kubr2017@gmail.com>
2019-01-01 19:38:20 -0500

    feat: Built ShelfFrame component

---
+++ src/components/ShelfFrame.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {ShelfInner} from './ShelfInner'
+import {Book} from './Book'
+
+class ShelfFrame extends React.Component {
+  render (){
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+
+      </div>
+    )
+  }
+}
