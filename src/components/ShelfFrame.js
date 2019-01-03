a642959ccf84e33cbcac45808a6d04a09999db79
kubr2017 <kubr2017@gmail.com>
2019-01-03 10:45:07 -0500

    fix: fix bugs.
    Bookshelf-title didnot display in <ShelfFrame/>

--- src/components/ShelfFrame.js
+++ src/components/ShelfFrame.js
@@ -5,7 +5,7 @@ class ShelfFrame extends React.Component {
   render (){
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{(this.props.booksArr.shelf==='currentlyReading'&&this.props.shelfs[0])||(this.props.booksArr.shelf==='wantToRead'&&this.props.shelfs[1])||(this.props.booksArr.shelf==='read'&&this.props.shelfs[2])}</h2>
+        <h2 className="bookshelf-title">{this.props.currentShelf}</h2>
           <ShelfInner shelfs = {this.props.shelfs} booksArr = {this.props.booksArr}/>
       </div>
     )
