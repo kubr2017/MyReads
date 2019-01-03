2732f2340363f3a850999ce77cb1a40f72b9bfe8
kubr2017 <kubr2017@gmail.com>
2019-01-03 13:04:59 -0500

    feat: implement savings of DropDownList current state

--- src/components/DropDownList.js
+++ src/components/DropDownList.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 class DropDownList extends React.Component {
-  changeFunc(e){
-    console.log('option fired',e.currentTarget.value);
+  changeFunc = (e) => {
+    console.log('option fired:',e.currentTarget.value);
+    this.setState({value:e.currentTarget.value});
     //(options&&console.log('value in DropDown:',e.options[e.selected.index].value))||console.log('options:'+undefined)
     //this.props.moveBook(e.options[e.selected.index].value);
   }
+
+  state = {value: this.props.currentShelf};
+
   renderDropDownList = (dropDownDownData) => {
     return (
       <div className="book-shelf-changer">
-        <select value = {this.props.currentShelf} onChange={this.changeFunc}>
+        <select value = {this.state.value} onChange={this.changeFunc}>
           <option value="move" disabled>Move to...</option>
 
           {dropDownDownData.map((item) => (<option key = {item} value={item} >{item}</option>))}
